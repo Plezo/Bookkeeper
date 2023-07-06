2368ac8ded64a6d69a4812e7f031bf45e9ad632d
Plezo <Plezo@protonmail.com>
2023-07-05 21:07:50 -0400

    Implemented api data visually

--- app/stock/[ticker]/page.jsx
+++ app/stock/[ticker]/page.jsx
@@ -1,24 +1,70 @@
-import { getBalanceSheet, getIncomeSheet, getCashFlowStatement } from '@/lib/fmp'
-import { camelToTitle } from '@/lib/utils'
+import {
+  getBalanceSheet,
+  getIncomeSheet,
+  getCashFlowStatement,
+} from "@/lib/fmp";
+import { camelToTitle, formatNum } from "@/lib/utils";
+
+// probably move this to a utils file or parse it in fmp.js initially
+const balanceSheetExclude = new Set([
+  "date",
+  "symbol",
+  "reportedCurrency",
+  "cik",
+  "fillingDate",
+  "acceptedDate",
+  "calendarYear",
+  "period",
+  "link",
+  "finalLink",
+]);
 
 async function Stock({ params }) {
+  const data = await getBalanceSheet(params?.ticker);
 
-    const data = await getBalanceSheet(params?.ticker);
-    
-    return (
-    <div className='p-12 overflow-y-scroll scrollbar-hide h-screen'>
-        <section>
+  return (
+    <div className="p-12 overflow-y-scroll scrollbar-hide h-screen">
+      <section className=" space-y-8">
+        <h1 className="text-3xl text-center">
+          Balance Sheet for {params?.ticker}
+        </h1>
+        <table className="whitespace-nowrap border-separate border-spacing-6 text-sm font-light">
+          <thead className="border-b font-medium border-neutral-500">
+            <tr>
+              <th scope="col" className="">
+                All values USD Millions
+              </th>
+              {data.map((obj, i) => {
+                return (
+                  <th key={i} scope="col" className=" text-left w-8">
+                    {obj.date.slice(0, 4)}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
             {Object.entries(data[0]).map(([key, value]) => {
-            return (
-                <div key={key} className='grid grid-cols-2'>
-                <h1>{camelToTitle(key)}:</h1>
-                <p>{value}</p>
-                </div>
-            )
+              if (!balanceSheetExclude.has(key)) {
+                return (
+                  <tr key={key} className="">
+                    <td className="font-medium">{camelToTitle(key)}</td>
+                    {data.map((obj, i) => {
+                      return (
+                        <td key={i} className="">
+                          {formatNum(obj[key] / 1000000)}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              }
             })}
-        </section>
+          </tbody>
+        </table>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Stock
+export default Stock;
